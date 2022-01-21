259fbb7a2af7b7a192355f097315092f8eb386fe
GlenMerlin <glenmerlin@protonmail.com>
2022-01-20 18:16:49 -0700

    most of the code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,72 @@
 #include <iostream>
+#include <string>
+#include <stack>
 
 using namespace std;
 
-int main(){
-    return 0;
+int main(int argc, char *argv[]){
+    string input = argv[1];
+    stack<char> parenthesis, angle, curly, square;
+    
+    bool failed;
+    while (!failed){
+            for (char& groupChar : input){
+            switch(groupChar){
+
+                case ('('): {
+                    parenthesis.push(groupChar);
+                }
+                case ('<'): {
+                    angle.push(groupChar);
+                }
+                case ('{'): {
+                    curly.push(groupChar);
+                }
+                case ('['): {
+                    square.push(groupChar);
+                }
+                case (')'): {
+                    if (!parenthesis.empty()){
+                        parenthesis.pop();
+                    }
+                    else {
+                        failed = true;
+                    };
+                }
+                case ('>'): {
+                    if (!angle.empty()){
+                        angle.pop();
+                    }
+                    else {
+                        failed = true;
+                    };
+                }
+                case ('}'): {
+                    if (!curly.empty()){
+                        curly.pop();
+                    }
+                    else {
+                        failed = true;
+                    };
+                }
+                case (']'): {
+                    if (!square.empty()){
+                        square.pop();
+                    }
+                    else {
+                        failed = true;
+                    };
+                }
+                default: {
+                    cerr << "unknown input" << endl;
+                }
+            }
+        }
+    }
+    if (!failed){
+        cout << "true" << endl;
+    }
+    else {
+        cout << "false" << endl;
+    }   
 }
